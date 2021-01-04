939f2f44e21957646f7ccbee2cc2ebb058a74a47
Baxtiyor-UZB <uzbekpy@gmail.com>
2021-01-04 11:10:43 +0500

    Signed-off-by: Baxtiyor-UZB <uzbekpy@gmail.com>

---
+++ maker.py
@@ -0,0 +1,43 @@
+from projects.models import Project
+p1 = Project(
+    title='Informatika V axborot texnalogiyalar',
+    description="Fan yuzasidan ma'lumotlar",
+    technology='Django',
+    image='img/project1.png'
+    )
+p1.save()
+p2 = Project(
+    title='Algebra',
+    description="Fan yuzasidan ma'lumotlar",
+    technology='Django',
+    image='img/project1.png'
+    )
+p2.save()
+p3 = Project(
+    title='Geometriya',
+    description="Fan yuzasidan ma'lumotlar",
+    technology='Django',
+    image='img/project1.png'
+    )
+p3.save()
+p4 = Project(
+    title='HTML',
+    description="Fan yuzasidan ma'lumotlar",
+    technology='Django',
+    image='img/project1.png'
+    )
+p4.save()
+p5 = Project(
+    title='CSS',
+    description="Fan yuzasidan ma'lumotlar",
+    technology='Django',
+    image='img/project1.png'
+    )
+p5.save()
+p6 = Project(
+    title='PYTHON',
+    description="Fan yuzasidan ma'lumotlar",
+    technology='Django',
+    image='img/project1.png'
+    )
+p6.save()
